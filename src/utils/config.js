d5520f3e4737a032fbb6e279f7dc5eed7c9a3ab1
Ben Naylor <nayben@gmail.com>
2023-03-19 12:26:10 +0100

    Updating subtitle to something a bit more me

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const config = {
   title: 'Ben Naylor - Curriculum Vitae',
   contact: {
     title: 'Ben Naylor',
-    subtitle: 'Professional Code Monkey, General Enthusiast',
+    subtitle: 'Software Engineer, Power Nerd, Enthusiast about Enthusiasts',
     email: 'nayben@gmail.com',
     phone: '+31 6 1427 2332'
   }
